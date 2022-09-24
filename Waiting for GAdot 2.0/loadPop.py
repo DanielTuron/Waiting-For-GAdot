7959144665cd0119de3ddb0621483199c7f984c8
DanielTuron <114313861+DanielTuron@users.noreply.github.com>
2022-09-24 18:28:24 -0500

    Delete Waiting for GAdot 2.0 directory

--- Waiting for GAdot 2.0/loadPop.py
+++
@@ -1,6 +0,0 @@
-import pickle
-
-file = open("pointillism2.0Nemo.obj","rb")
-P = pickle.load(file)
-
-P.continueInterface()
--- Waiting for GAdot 2.0/testGenome.py
+++
@@ -1,9 +0,0 @@
-from Organism import Genome
-import cv2
-realIm = cv2.imread("farmSample.jpg")
-gg = Genome()
-f = gg.getFitness(realIm)
-print(f)
-
-
-gg.makeImage()
