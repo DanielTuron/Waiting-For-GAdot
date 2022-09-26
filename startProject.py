40f8ef37510e632acaff254a3a1f85eb54b6b66b
Daniel Turon <turond@myumanitoba.ca>
2022-09-25 23:17:16 -0500

    Update startProject.py

--- startProject.py
+++ startProject.py
@@ -1,16 +1,23 @@
-import sys
 import time
 from Population import Population
 import geneticFunctions.mutationFunction as mf
 import geneticFunctions.breedingFunction as bf
 import geneticFunctions.selectionFunction as sf
 import geneticFunctions.fitnessFunction as ff
-import os
 
+# Adjust these to use wanted genetic functions
+fitnessFunction = ff.labDistance
+mutationFunction = mf.myMutate
+breedingFunction = bf.breed
+selectionFunction = sf.tournamentSelection
 
+# Create your project folder, and move the image into the folder
+projectFolderName = 'MoonLight'
+refrenceImageName = 'MoonLight.jpg'
+pickleName = "pop2.obj"
 
 start_time = time.time()
-P = Population("MoonLight.jpg",mf.myMutate,bf.breed,sf.tournamentSelection,ff.labDistance,'MoonLight')
+P = Population(refrenceImageName,mutationFunction,breedingFunction,selectionFunction,fitnessFunction,projectFolderName,pickleName=pickleName)
 print("gen 0")
 print("fitness: ",P.population[0].fitness)
 print("--- %s seconds ---" % (time.time() - start_time))
